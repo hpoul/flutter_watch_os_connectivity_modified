d25ac1e177c35f20c7d0553d98f694ddf66749d6
chuanpham <pvchuan2998@gmail.com>
2023-12-06 11:30:30 +0700

    Update SwiftFlutterWatchOsPlugin.swift

--- ios/Classes/SwiftFlutterWatchOsPlugin.swift
+++ ios/Classes/SwiftFlutterWatchOsPlugin.swift
@@ -294,16 +294,18 @@ extension SwiftFlutterWatchOsConnectivityPlugin: WCSessionDelegate{
     }
     
     public func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
-        DispatchQueue.main.async {
-            self.callbackChannel.invokeMethod("onApplicationContextUpdated", arguments: getApplicationContext(session: session))
+        DispatchQueue.main.async { [weak self] in
+            guard let self else { return }
+            self?.callbackChannel.invokeMethod("onApplicationContextUpdated", arguments: getApplicationContext(session: session))
         }
         
     }
     
     public func sessionWatchStateDidChange(_ session: WCSession) {
         do {
-            DispatchQueue.main.async {
-                self.callbackChannel.invokeMethod("pairDeviceInfoChanged", arguments: try session.toPairedDeviceJsonString())
+            DispatchQueue.main.async { [weak self] in
+                guard let self else { return }
+                self?.callbackChannel.invokeMethod("pairDeviceInfoChanged", arguments: try session.toPairedDeviceJsonString())
             }
         } catch {
             handleCallbackError(message: error.localizedDescription)
@@ -379,8 +381,9 @@ extension SwiftFlutterWatchOsConnectivityPlugin: WCSessionDelegate{
 extension SwiftFlutterWatchOsConnectivityPlugin{
     private func getPairedDeviceInfo(session: WCSession){
         do {
-            DispatchQueue.main.async {
-                self.callbackChannel.invokeMethod("pairDeviceInfoChanged", arguments: try session.toPairedDeviceJsonString())
+            DispatchQueue.main.async { [weak self] in
+                guard let self else { return }
+                self?.callbackChannel.invokeMethod("pairDeviceInfoChanged", arguments: try session.toPairedDeviceJsonString())
             }
         } catch {
             handleCallbackError(message: error.localizedDescription)
